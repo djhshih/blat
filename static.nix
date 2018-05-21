c9a741873516aeea5df3be003b272dd1cd4763bb
David J. H. Shih <djh.shih@gmail.com>
2018-05-21 11:01:33 -0400

    Disable hardening.

--- static.nix
+++ static.nix
@@ -3,4 +3,5 @@ stdenv.mkDerivation rec {
 	name = "env";
 	env = buildEnv { name = name; paths = buildInputs; };
 	buildInputs = [ glibc.static coreutils libpng ];
+	hardeningDisable = [ "all" ];
 }
